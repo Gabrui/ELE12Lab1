a6e00b0e593f84f5dc4a5024dbb8e7c2faf69c3d
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2017-05-28 16:02:14 -0300

    Versão Final Cancerizada nos cálculos

--- cartaSmith.js
+++ cartaSmith.js
@@ -1,10 +1,17 @@
+/* global math */
+
 //UTILIZA FUNÇÕES DO ARQUIVO codigo.js
 
 /**
  * @class Representação gráfica da Carta de Smith
+ * @param {Number} cx A coordenada x do centro da carta de smith da imagem
+ * @param {Number} cy A coordenada y do centro da carta de smith da imagem
+ * @param {Number} r O raio da carta de smith da imagem
+ * @param {string} desenho O ID do canvas no html
+ * @param {string} fundo O ID da imagem de fundo do html
  * @returns {CartaSmith} Um objeto do tipo carta de smith
  */
-CartaSmith = function () {
+CartaSmith = function (cx, cy, r, desenho, fundo) {
     
     /**
      * 
@@ -18,9 +25,34 @@ CartaSmith = function () {
         this.cx = cx; ///< Posição x do centro da figura
         this.cy = cy; ///< Posição y do centro da figura
         this.r = r;   ///< Raio da carta de smith da figura
-        this.desenho = document.getElementById(desenho).getContext("2d");
+        this.canvas = document.getElementById(desenho);
+        this.desenho = this.canvas.getContext("2d");
         this.fundo = document.getElementById(fundo);
+        this.desenho.restore();
         this.apagar();
+        this.desenho.moveTo(0, 0);
+        this.setCor("#008800");
+        this.desenho.lineWidth = 2;
+        this.fonte = "'Open Sans', sans-serif";
+        this.desenho.font = "bold 3em 'Open Sans', sans-serif";
+    };
+    
+    /**
+     * Muda a cor de desenho dos pontos e retas da carta
+     * @param {string} cor A representação hexadecimal ou de nome da cor
+     */
+    this.setCor = function(cor) {
+        this.desenho.fillStyle = cor;
+        this.desenho.strokeStyle = cor;
+    };
+    
+    
+    /**
+     * Muda a fonte
+     * @param {type} fonte A representação da fonte
+     */
+    this.setFonte = function(fonte) {
+        this.desenho.font = "bold " + fonte + this.fonte;
     };
     
     
@@ -28,6 +60,7 @@ CartaSmith = function () {
      * @function apagar Apaga que foi desenhado
      */
     this.apagar = function() {
+        this.desenho.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.desenho.drawImage(this.fundo, 0, 0);
     };
     
@@ -35,12 +68,21 @@ CartaSmith = function () {
     /**
      * @function desenharPontoZNorm Desenha um ponto de impedância normalizada
      * na carta de smith.
-     * @param {math.complex} zComplexo Impedância normalizada
+     * @param {math.complex} zNormComplexo Impedância normalizada
      * @param {string} texto Comentário sobre o ponto
      */
-    this.desenharPontoZNorm = function(zComplexo, texto) {
-        desenharR(calcularReflexao(zComplexo));
-        //IMPLEMENTAR CÓDIGO QUE ESCREVE O TEXTO SOBRE O PONTO
+    this.desenharPontoZNorm = function(zNormComplexo, texto) {
+        this.desenharR(calcularReflexao(zNormComplexo), texto);
+    };
+    
+    
+    /**
+     * @function desenharRetaR Desenha uma reta que liga o centro da carta de 
+     * smith até o ponto que representa uma impedância normalizada.
+     * @param {math.complex} zNormComplexo Impedância normalizada
+     */
+    this.desenharRetaZNorm = function(zNormComplexo) {
+        this.desenharRetaR(calcularReflexao(zNormComplexo));
     };
     
     
@@ -48,11 +90,17 @@ CartaSmith = function () {
      * @function desenharR Desenha um ponto na carta de Smith referente a o
      * coeficiente de reflexão dado
      * @param {math.complex} reflexaoComplexo Coeficiente de reflexão
+     * @param {string} texto Texto a ser legenda do ponto
      */
-    this.desenharR = function(reflexaoComplexo) {
-        var x = reflexaoComplexo.re*this.r + this.cx; ///< Posição x na figura
-        var y = reflexaoComplexo.im*this.r + this.cy; ///< Posição y na figura
-        //IMPLEMENTAR O RESTO
+    this.desenharR = function(reflexaoComplexo, texto) {
+        var x =  reflexaoComplexo.re*this.r + this.cx; // Posição x na figura
+        var y = -reflexaoComplexo.im*this.r + this.cy; // Posição y na figura
+        this.desenho.beginPath();
+        this.desenho.arc(x, y, this.r/100, 0, 2*Math.PI);
+        this.desenho.fill();
+        if (texto) {
+            this.desenho.fillText(texto, x + 10, y - 10); 
+        }
     };
     
     
@@ -62,24 +110,54 @@ CartaSmith = function () {
      * @param {math.complex} reflexaoComplexo Coeficiente de reflexão
      */
     this.desenharRetaR = function(reflexaoComplexo) {
-        //IMPLEMENTAR
+        var x =  reflexaoComplexo.re*this.r + this.cx; // Posição x na figura
+        var y = -reflexaoComplexo.im*this.r + this.cy; // Posição y na figura
+        this.desenho.beginPath();
+        this.desenho.moveTo(this.cx, this.cy);
+        this.desenho.lineTo(x, y);
+        this.desenho.stroke(); 
     };
     
     
     /**
      * 
-     * @param {math.complex} rInicial Coeficiente de Reflexão que marca o início
-     * da interpolação
-     * @param {math.complex} rFinal Coeficiente de Reflexão que marca o final
-     * da interpolação
+     * @param {math.complex} zNormInicial Z Normalizado inicial
+     * @param {math.complex} zNormFinal Z Normalizado final
      */
-    this.interpolarR = function(rInicial, rFinal) {
-        //IMPLEMENTAR
-        //Pensei em colocar um for que usa o desenharR, ou pelo menos aproveita 
-        //parte dele, utilizando um passo fixo
+    this.interpolarZ = function(zNormInicial, zNormFinal) {
+        var i = 0;
+        var iteracoes = 100;
+        var delta = math.divide(math.subtract(zNormFinal, zNormInicial), iteracoes);
+        var temp = zNormInicial.clone();
+        var rTemp = calcularReflexao(temp);
+        var x =  rTemp.re*this.r + this.cx; // Posição x na figura
+        var y = -rTemp.im*this.r + this.cy; // Posição y na figura
+        this.desenho.beginPath();
+        this.desenho.moveTo(x, y);
+        for(i = 0; i<iteracoes; i++) {
+            temp = math.add(temp, delta);
+            rTemp = calcularReflexao(temp);
+            x =  rTemp.re*this.r + this.cx; // Posição x na figura
+            y = -rTemp.im*this.r + this.cy; // Posição y na figura
+            this.desenho.lineTo(x, y);
+        }
+        this.desenho.stroke();
     };
     
+    
+    /**
+     * Desenha uma curva, segmento de circunferência de Coeficiente de Reflexão
+     * constante.
+     * @param {math.complex} rInicial Marca o ponto de início, é o seu raio que
+     * é utilizado
+     * @param {math.complex} rFinal Marca o ponto de final
+     */
+    this.curvaRConst = function(rInicial, rFinal) {
+        this.desenho.beginPath();
+        this.desenho.arc(this.cx, this.cy, rInicial.toPolar().r * this.r, 
+            -rInicial.toPolar().phi, -rFinal.toPolar().phi);
+        this.desenho.stroke();
+    };
+    
+    this.iniciar(cx, cy, r, desenho, fundo);
 };
-
-
-carta = new CartaSmith();
--- codigo.js
+++ codigo.js
@@ -1,6 +1,8 @@
 //UTILIZA A BIBLIOTECA math.js para o cálculo com números complexos
 
-/* global math, carta */
+/* global math, MathJax, Plotly */
+
+var epsilon = math.pow(10, -8);
 
 /**
  * @function somarSeNegativo Soma um incremento a um número se este for negativo
@@ -73,99 +75,290 @@ function calcularVswr(R) {
 
 /**
  * 
- * @param {math.complex} zlComplexo O
+ * @param {math.complex} zlComplexo A impedância da carga normalizada.
  * @returns {math.complex} O coeficiente de reflexão complexo
  */
 function calcularReflexao(zlComplexo) {
-    return math.divide(math.subtract(zlComplexo, Z0), math.add(Z0, zlComplexo));
+    return math.divide(math.subtract(zlComplexo, 1), math.add(1, zlComplexo));
 }
 
 
+
+/**
+ * Gira no sentido horário para o cáculo da diferença de ângulo
+ * @param {number} inicio Ângulo inicial entre -pi e pi
+ * @param {number} fim Ângulo final entre -pi e pi
+ * @returns {number} Distância angular a ser percorrida no sentido horário
+ */
+function calcDifAngRad(inicio, fim) {
+    // Tem que passar pela descontinuidade
+    if (inicio < fim) {
+        return 2*math.pi - (fim - inicio);
+    } else {
+        return inicio - fim;
+    }
+}
+
+
+
 function calcular() {
-    var u0 = 4 * math.pi * math.pow(10, -7);
-    var e0 = 8.8541878176 * math.pow(10, -12);
-    
+
     var ReZ0 = document.getElementsByName("ReZ0")[0].value;
     var ImZ0 = 0;//document.getElementsByName("ImZ0")[0].value;
-    Z0 = math.complex(ReZ0, ImZ0);
+    var Z0 = math.complex(ReZ0, ImZ0);
     var ReZL = document.getElementsByName("ReZL")[0].value;
     var ImZL = document.getElementsByName("ImZL")[0].value;
     var ZL = math.complex(ReZL, ImZL);
     var freq = document.getElementsByName("freq")[0].value * math.pow(10, 6);
     var urel = document.getElementsByName("urel")[0].value;
+    var erel = document.getElementsByName("erel")[0].value;
     var bw   = document.getElementsByName("bw")  [0].value * math.pow(10,6);
     
+    var u = urel * 4 * math.pi * math.pow(10, -7);
+    var e = erel * 8.8541878176 * math.pow(10, -12);
+    // Impedância Normalizada da carga
     var ZLNorm = math.divide(ZL, Z0);
+    // Admitância Normalizada da carga
     var YLNorm = math.divide(1, ZLNorm);
+    // Comprimento de onda
+    var lambda = 1/(math.sqrt(u*e)*freq);
+    // Coeficiente de reflexão do toco em curto normalizada
+    var rTocoCurto = math.complex(-1, 0);
+    // Admitância do toco em aberto normalizada
+    var rTocoAberto = math.complex(1, 0);
+    // Frequência máxima da banda de operação
+    var fmax = freq + bw/2;
+    // Frequência mínima da banda de operação
+    var fmin = freq - bw/2;
+    var freqs = [fmin, fmax];
+
+    
+    /* CÓDIGO PARA CALCULAR PELA CARTA DE SMITH
+    for (var numSol = 1; numSol <=2; numSol++) {
+        // Coeficiente de Reflexão da carga, complexo
+        var reflexaoCarga = calcularReflexao(ZLNorm);
+        // O seu módulo
+        var reflexaoCargaModulo = reflexaoCarga.toPolar().r;
+        // O coeficiente de reflexão no ponto de parte real da admitância igual
+        // a 1. Ele é simétrico com relação a outra solução
+        var reflexaoParteReal1 = math.complex.fromPolar(reflexaoCargaModulo,
+            math.acos(reflexaoCargaModulo) * ((numSol===1)?1:-1));
+        // A admitância desse ponto de reflexão
+        var admitanciaParteReal1 = math.divide(math.add(1, reflexaoParteReal1), 
+            math.subtract(1, reflexaoParteReal1));
+        // Um coeficiente de reflexão associada à admitância da carta de smith
+        var reflexaoAdmitancia = calcularReflexao(YLNorm);
+        // A distância elétrica
+        var xEletrico = calcDifAngRad(reflexaoAdmitancia.toPolar().phi,
+                       reflexaoParteReal1.toPolar().phi)*0.5/(2*math.pi);
+        // A distância real
+        var x = xEletrico * lambda;
+        // Comprimento do Toco em curto
+        var compTocoCurto = calcDifAngRad(rTocoCurto.toPolar().phi,
+            -reflexaoParteReal1.toPolar().phi)*lambda*0.5/(2*math.pi);
+        // Comprimento do Toco em aberto
+        var compTocoAberto = calcDifAngRad(rTocoAberto.toPolar().phi,
+            -reflexaoParteReal1.toPolar().phi)*lambda*0.5/(2*math.pi);
+        
+        // Descasamento de 
+        for (var tam = freqs.length - 1; tam >= 0; tam--) {
+            
+        }
+    }*/
     
     var a = YLNorm.re;
     var b = YLNorm.im;
     
+
     var A = a*a + b*b - a;
     var B = -2*b;
     var C = 1 - a;
     var delta = B*B - 4*A*C;
-    var t1 = (-B + math.sqrt(delta))/(2*A);
-    var t2 = (-B - math.sqrt(delta))/(2*A);
-    var lambda = 1/(math.sqrt(urel*u0*e0)*freq);
-    var x1 = somarSeNegativo(lambda/2, lambda * math.atan(t1)/(2*math.pi));
-    var x2 = somarSeNegativo(lambda/2, lambda * math.atan(t2)/(2*math.pi));
-    
-    
-    var Imy1 = ((b+t1)*(1-b*t1)-t1*a*a)/((1-b*t1)*(1-b*t1)+(t1*a)*(t1*a));
-    var Imy2 = ((b+t2)*(1-b*t2)-t2*a*a)/((1-b*t2)*(1-b*t2)+(t2*a)*(t2*a));
-    
+    if (math.abs(A) < epsilon) {
+        var t2 = -C/B;
+
+        var x1 = lambda/4;
+        var x2 = somarSeNegativo(lambda/2, lambda * math.atan(t2)/(2*math.pi));
+
+        var Imy1 =  ZLNorm.im;
+        var Imy2 = -ZLNorm.im;
+    } else {
+        var t1 = (-B + math.sqrt(delta))/(2*A);
+        var t2 = (-B - math.sqrt(delta))/(2*A);
+        
+        var x1 = somarSeNegativo(lambda/2, lambda * math.atan(t1)/(2*math.pi));
+        var x2 = somarSeNegativo(lambda/2, lambda * math.atan(t2)/(2*math.pi));
+
+        var Imy1 = ((b+t1)*(1-b*t1)-t1*a*a)/((1-b*t1)*(1-b*t1)+(t1*a)*(t1*a));
+        var Imy2 = ((b+t2)*(1-b*t2)-t2*a*a)/((1-b*t2)*(1-b*t2)+(t2*a)*(t2*a));
+    }
+
+
     var comp1aberto = somarSeNegativo(lambda/2, math.atan(Imy1)*lambda/(2*math.pi));
     var comp1curto = somarSeNegativo(lambda/2, math.atan(-1/Imy1)*lambda/(2*math.pi));
     var comp2aberto = somarSeNegativo(lambda/2, math.atan(Imy2)*lambda/(2*math.pi));
     var comp2curto = somarSeNegativo(lambda/2, math.atan(-1/Imy2)*lambda/(2*math.pi));
     
-    var fmax = freq + bw/2;
-    var fmin = freq - bw/2;
-    var NyTaberto1max = calcularNovoyToco(true,comp1aberto,fmax,u0,e0);
-    var NyTaberto2max = calcularNovoyToco(true,comp2aberto,fmax,u0,e0);
-    var NyTcurto1max = calcularNovoyToco(false,comp1curto,fmax,u0,e0);
-    var NyTcurto2max = calcularNovoyToco(false,comp2curto,fmax,u0,e0);
-    var NyTaberto1min = calcularNovoyToco(true,comp1aberto,fmin,u0,e0);
-    var NyTaberto2min = calcularNovoyToco(true,comp2aberto,fmin,u0,e0);
-    var NyTcurto1min = calcularNovoyToco(false,comp1curto,fmin,u0,e0);
-    var NyTcurto2min = calcularNovoyToco(false,comp2curto,fmin,u0,e0);
     
-    var R1abertomax = calcularModR(a,b,NyTaberto1max,x1,fmax,u0,e0);
-    var R2abertomax = calcularModR(a,b,NyTaberto2max,x2,fmax,u0,e0);
-    var R1abertomin = calcularModR(a,b,NyTaberto1min,x1,fmin,u0,e0);
-    var R2abertomin = calcularModR(a,b,NyTaberto2min,x2,fmin,u0,e0);
-    
-    var R1curtomax = calcularModR(a,b,NyTcurto1max,x1,fmax,u0,e0);
-    var R2curtomax = calcularModR(a,b,NyTcurto2max,x2,fmax,u0,e0);
-    var R1curtomin = calcularModR(a,b,NyTcurto1min,x1,fmin,u0,e0);
-    var R2curtomin = calcularModR(a,b,NyTcurto2min,x2,fmin,u0,e0);
-    
-    var Vswr1abertomax = calcularVswr(R1abertomax);
-    var Vswr2abertomax = calcularVswr(R2abertomax);
-    var Vswr1abertomin = calcularVswr(R1abertomin);
-    var Vswr2abertomin = calcularVswr(R2abertomin);
-    
-    var Vswr1curtomax = calcularVswr(R1curtomax);
-    var Vswr2curtomax = calcularVswr(R2curtomax);
-    var Vswr1curtomin = calcularVswr(R1curtomin);
-    var Vswr2curtomin = calcularVswr(R2curtomin);
+    var f = fmin;
+    var quantIter = 50;
+    var df = bw/quantIter;
+    var xGrafico = [];
+    var Vswr1aberto = [];
+    var Vswr2aberto = [];
+    var Vswr1curto = [];
+    var Vswr2curto = [];
+    for (var quant = 0; quant <= quantIter; quant++, f+=df) {
+        var NyTaberto1 = calcularNovoyToco(true,comp1aberto,f,u,e);
+        var NyTaberto2 = calcularNovoyToco(true,comp2aberto,f,u,e);
+        var NyTcurto1 = calcularNovoyToco(false,comp1curto,f,u,e);
+        var NyTcurto2 = calcularNovoyToco(false,comp2curto,f,u,e);
+
+        var R1aberto = calcularModR(a,b,NyTaberto1,x1,f,u,e);
+        var R2aberto = calcularModR(a,b,NyTaberto2,x2,f,u,e);
+
+        var R1curto = calcularModR(a,b,NyTcurto1,x1,f,u,e);
+        var R2curto = calcularModR(a,b,NyTcurto2,x2,f,u,e);
+
+        xGrafico[quant] = f;
+        Vswr1aberto[quant] = calcularVswr(R1aberto);
+        Vswr2aberto[quant] = calcularVswr(R2aberto);
+        Vswr1curto[quant] = calcularVswr(R1curto);
+        Vswr2curto[quant] = calcularVswr(R2curto);
+    }
     
     var texto = "";
+    var carta1 = new CartaSmith(649, 649, 1196-648, "desenho", "fundo");
     
     texto += "<table>";
+    texto += "<caption>Primeira Solução</caption>";
+    texto += "<tr><td>$\\text{Comprimento de Onda: }$</td><td>$" + lambda.toFixed(4) + "\\;m $</td></tr>";
+    texto += "<tr><td>$\\text{Impedância Normalizada: }$</td><td>$" + ZLNorm.re.toFixed(4) +" + j\\;" + ZLNorm.im.toFixed(4) + "$</td></tr>";
+    carta1.desenharRetaZNorm(ZLNorm);
+    carta1.desenharPontoZNorm(ZLNorm, "zL");
+    texto += "<tr><td>$\\text{Admitância Normalizada: }$</td><td>$" + YLNorm.re.toFixed(4) +" + j\\;" + YLNorm.im.toFixed(4) + "$</td></tr>";
+    carta1.setCor("#008888");
+    carta1.desenharRetaZNorm(YLNorm);
+    carta1.desenharPontoZNorm(YLNorm, "yL");
+    carta1.setCor("#FF0000");
+    carta1.curvaRConst(calcularReflexao(YLNorm), calcularReflexao(math.complex(1, Imy1)));
+    carta1.setFonte("1em");
+    carta1.desenharPontoZNorm(math.complex(1, Imy1), "Parte Real 1");
+    carta1.setCor("#FF33FF");
+    carta1.interpolarZ(math.complex(1, Imy1), math.complex(1, 0));
+    texto += "<tr><td>$\\text{Distância do Toco à carga: }$</td><td>$" + x1.toFixed(4) +"\\;m $</td></tr>";
+    texto += "<tr><td>$\\text{Comprimento do Toco em aberto: }$</td><td>$" + comp1aberto.toFixed(4) +"\\;m $</td></tr>";
+    texto += "<tr><td>$\\text{Comprimento do Toco em curto: }$</td><td>$" + comp1curto.toFixed(4) +"\\;m $</td></tr>";
+    texto +="<tr><td>$V_{swr} \\text{ em aberto para frequencia máxima: }$</td><td>$"+Vswr1aberto[quantIter].toFixed(4)+"\\;\\; $</td></tr>";
+    texto +="<tr><td>$V_{swr} \\text{ em aberto para frequencia mínima: }$</td><td>$"+Vswr1aberto[0].toFixed(4)+"\\;\\; $</td></tr>";
+    texto +="<tr><td>$V_{swr} \\text{ em curto para frequencia máxima: }$</td><td>$"+Vswr1curto[quantIter].toFixed(4)+"\\;\\; $</td></tr>";
+    texto +="<tr><td>$V_{swr} \\text{ em curto para frequencia mínima: }$</td><td>$"+Vswr1curto[0].toFixed(4)+"\\;\\; $</td></tr>";
+    texto += "</table>";
+    
+    
+    document.getElementById("respostas").innerHTML = texto;
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub,"respostas"]);
+    
+    
+    var texto2 = "";
+    var carta2 = new CartaSmith(649, 649, 1196-648, "desenho2", "fundo");
+    texto2 += "<table>";
+    texto2 += "<caption>Segunda Solução</caption>";
+    texto2 += "<tr><td>$\\text{Comprimento de Onda: }$</td><td>$" + lambda.toFixed(4) + "\\;m $</td></tr>";
+    texto2 += "<tr><td>$\\text{Impedância Normalizada: }$</td><td>$" + ZLNorm.re.toFixed(4) +" + j\\;" + ZLNorm.im.toFixed(4) + "$</td></tr>";
+    carta2.desenharRetaZNorm(ZLNorm);
+    carta2.desenharPontoZNorm(ZLNorm, "zL");
+    texto2 += "<tr><td>$\\text{Admitância Normalizada: }$</td><td>$" + YLNorm.re.toFixed(4) +" + j\\;" + YLNorm.im.toFixed(4) + "$</td></tr>";
+    carta2.setCor("#008888");
+    carta2.desenharRetaZNorm(YLNorm);
+    carta2.desenharPontoZNorm(YLNorm, "yL");
+    carta2.setCor("#FF0000");
+    carta2.curvaRConst(calcularReflexao(YLNorm), calcularReflexao(math.complex(1, Imy2)));
+    carta2.setFonte("1em");
+    carta2.desenharPontoZNorm(math.complex(1, Imy2), "Parte Real 1");
+    carta2.setCor("#FF33FF");
+    carta2.interpolarZ(math.complex(1, Imy2), math.complex(1, 0));
+    texto2 += "<tr><td>$\\text{Distância do Toco à carga: }$</td><td>$" + x2.toFixed(4) +"\\;m $</td></tr>";
+    texto2 += "<tr><td>$\\text{Comprimento do Toco em aberto: }$</td><td>$" + comp2aberto.toFixed(4) +"\\;m $</td></tr>";
+    texto2 += "<tr><td>$\\text{Comprimento do Toco em curto: }$</td><td>$" + comp2curto.toFixed(4) +"\\;m $</td></tr>";
+    texto2 +="<tr><td>$V_{swr} \\text{ em aberto para frequencia máxima: }$</td><td>$"+Vswr2aberto[quantIter].toFixed(4)+"\\;\\; $</td></tr>";
+    texto2 +="<tr><td>$V_{swr} \\text{ em aberto para frequencia mínima: }$</td><td>$"+Vswr2aberto[0].toFixed(4)+"\\;\\; $</td></tr>";
+    texto2 +="<tr><td>$V_{swr} \\text{ em curto para frequencia máxima: }$</td><td>$"+Vswr2curto[quantIter].toFixed(4)+"\\;\\; $</td></tr>";
+    texto2 +="<tr><td>$V_{swr} \\text{ em curto para frequencia mínima: }$</td><td>$"+Vswr2curto[0].toFixed(4)+"\\;\\; $</td></tr>";
+    texto2 += "</table>";
+    
+    
+    document.getElementById("respostas2").innerHTML = texto2;
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub,"respostas2"]);
+
+    
+    /*
+     *     texto += "<table>";
+    texto += "<caption>Primeira Solução</caption>";
     texto += "<tr><td>$\\text{Impedância Normalizada: }$</td><td>$" + ZLNorm.re +" + j\\;" + ZLNorm.im + "$</td></tr>";
     texto += "<tr><td>$\\text{Distância do Toco à carga: }$</td><td>$" + x1.toFixed(4) +"\\;m\\;\\; \\text{ou} \\;\\;" + x2.toFixed(4) + "\\;m $</td></tr>";
     texto += "<tr><td>$\\text{Comprimento do Toco em aberto: }$</td><td>$" + comp1aberto.toFixed(4) +"\\;m\\;\\; \\text{ou}\\;\\; " + comp2aberto.toFixed(4) + "\\;m $</td></tr>";
     texto += "<tr><td>$\\text{Comprimento do Toco em curto: }$</td><td>$" + comp1curto.toFixed(4) +"\\;m\\;\\; \\text{ou} \\;\\;" + comp2curto.toFixed(4) + "\\;m $</td></tr>";
-    texto +="<tr><td>$V_{swr} \\text{ em aberto para frequencia máxima: }$</td><td>$"+Vswr1abertomax.toFixed(4)+"\\;m\\;\\; \\text{ou} \\;\\;"+Vswr2abertomax.toFixed(4) + "\\;m $</td></tr>";
-    texto +="<tr><td>$V_{swr} \\text{ em aberto para frequencia mínima: }$</td><td>$"+Vswr1abertomin.toFixed(4)+"\\;m\\;\\; \\text{ou} \\;\\;"+Vswr2abertomin.toFixed(4) + "\\;m $</td></tr>";
-    texto +="<tr><td>$V_{swr} \\text{ em curto para frequencia máxima: }$</td><td>$"+Vswr1curtomax.toFixed(4)+"\\;m\\;\\; \\text{ou} \\;\\;"+Vswr2curtomax.toFixed(4) + "\\;m $</td></tr>";
-    texto +="<tr><td>$V_{swr} \\text{ em curto para frequencia mínima: }$</td><td>$"+Vswr1curtomin.toFixed(4)+"\\;m\\;\\; \\text{ou} \\;\\;"+Vswr2curtomin.toFixed(4) + "\\;m $</td></tr>";
+    texto +="<tr><td>$V_{swr} \\text{ em aberto para frequencia máxima: }$</td><td>$"+Vswr1abertomax.toFixed(4)+"\\;\\;\\; \\text{ou} \\;\\;"+Vswr2abertomax.toFixed(4) + "\\; $</td></tr>";
+    texto +="<tr><td>$V_{swr} \\text{ em aberto para frequencia mínima: }$</td><td>$"+Vswr1abertomin.toFixed(4)+"\\;\\;\\; \\text{ou} \\;\\;"+Vswr2abertomin.toFixed(4) + "\\; $</td></tr>";
+    texto +="<tr><td>$V_{swr} \\text{ em curto para frequencia máxima: }$</td><td>$"+Vswr1curtomax.toFixed(4)+"\\;\\;\\; \\text{ou} \\;\\;"+Vswr2curtomax.toFixed(4) + "\\; $</td></tr>";
+    texto +="<tr><td>$V_{swr} \\text{ em curto para frequencia mínima: }$</td><td>$"+Vswr1curtomin.toFixed(4)+"\\;\\;\\; \\text{ou} \\;\\;"+Vswr2curtomin.toFixed(4) + "\\; $</td></tr>";
     texto += "</table>";
-    document.getElementById("respostas").innerHTML = texto;
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub,"respostas"]);
-    carta.iniciar(648, 649, 1196-648, "desenho", "fundo");
-    carta.desenharPontozNorm(ZLNorm, "ZL");
+    
+     */
+    
+    var dadosVswr1a = {
+        x: xGrafico,
+        y: Vswr1aberto,
+        mode: 'lines',
+        name: 'Solução 1 Toco Aberto',
+        line: {shape: 'spline'},
+        type: 'scatter'
+    };
+    var dadosVswr2a = {
+        x: xGrafico,
+        y: Vswr2aberto,
+        mode: 'lines',
+        name: 'Solução 2 Toco Aberto',
+        line: {shape: 'spline'},
+        type: 'scatter'
+    };
+    var dadosVswr1c = {
+        x: xGrafico,
+        y: Vswr1curto,
+        mode: 'lines',
+        name: 'Solução 1 Toco em Curto',
+        line: {shape: 'spline'},
+        type: 'scatter'
+    };
+    var dadosVswr2c = {
+        x: xGrafico,
+        y: Vswr2curto,
+        mode: 'lines',
+        name: 'Solução 2 Toco em Curto',
+        line: {shape: 'spline'},
+        type: 'scatter'
+    };
+
+    var dados = [dadosVswr1a, dadosVswr1c, dadosVswr2a, dadosVswr2c];
+
+    var estilo = {
+        legend: {
+            y: 0.5,
+            font: {size: 16},
+            yref: 'paper'
+        },
+        title: 'Análise de Largura de faixa',
+        xaxis: {
+            title: 'Frequência (Hz)'
+        },
+        yaxis: {
+            title: 'SWR'
+        }
+    };
+
+    Plotly.newPlot('swr', dados, estilo);
+
+    
 }
 
